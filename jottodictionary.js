d22880e16a956114f9b1560d438d60a10e279d95
jqcr9 <jqc@jcrown.net>
2015-02-04 09:44:15 -0800

    Change over to /usr/share/dict/american-english, add that copyright notice.

--- jottodictionary.js
+++ jottodictionary.js
@@ -1,4 +1,26 @@
 
+// This wordlist extracted from /usr/share/dict/american-english on a current Ubuntu release
+// associated copyright notice there says this (in part):
+
+//  COPYRIGHT, SOURCES, and CREDITS:
+// 
+// The collective work is Copyright 2000-2004 by Kevin Atkinson as well
+// as any of the copyrights mentioned below:
+// 
+//   Copyright 2000-2004 by Kevin Atkinson
+// 
+//   Permission to use, copy, modify, distribute and sell these word
+//   lists, the associated scripts, the output created from the scripts,
+//   and its documentation for any purpose is hereby granted without fee,
+//   provided that the above copyright notice appears in all copies and
+//   that both that copyright notice and this permission notice appear in
+//   supporting documentation. Kevin Atkinson makes no representations
+//   about the suitability of this array for any purpose. It is provided
+//   "as is" without express or implied warranty.
+
+// more can be found here:
+// http://changelogs.ubuntu.com/changelogs/pool/main/s/scowl/scowl_6-3/wamerican-huge.copyright
+
 var dictionary_list = [
     "abets",
     "abhor",
